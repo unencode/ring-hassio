0eb82e506792854bbd441aeed001209ba5ab4b3b
isaacm@swicktech.com <isaacm@swicktech.com>
2020-12-27 17:22:48 -0600

    Testing images

--- ring_hassio/livestream.ts
+++ ring_hassio/livestream.ts
@@ -58,26 +58,18 @@ var camera = await getCamera()
 ///////////////
 
 console.log('Getting Snapshot...');
-var snapshot = camera.getSnapshot() // returns a Promise<Buffer> of the latest snapshot from the camera
-console.log('SNAPSHOT CONTAINS:' + snapshot);
+//var snapshot = camera.getSnapshot() // returns a Promise<Buffer> of the latest snapshot from the camera
+//console.log('SNAPSHOT CONTAINS:' + snapshot);
 
 //var response = snapshot.Read();
-const text = await readStream(snapshot);
-console.log('BUFFER CONTAINS:' + text);
+var text = await camera.getSnapshot()
+
+fs.writefile("lastSnapShot.jpg", text);
+
+
 }
 
-function readStream(stream, encoding = "utf8") {
-    
-  stream.setEncoding(encoding);
 
-  return new Promise((resolve, reject) => {
-      let data = "";
-      
-      stream.on("data", chunk => data += chunk);
-      stream.on("end", () => resolve(data));
-      stream.on("error", error => reject(error));
-  });
-}
 
 
 
